ddde39a3383da249ee6ec889a3d20e7d96c45ce6
thomas-w-w <thomas.westergard@gmail.com>
2024-04-26 20:02:20 +0200

    prototype tratits and struct

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # inheritance_macro
+
+https://stackoverflow.com/questions/59018413/when-to-use-self-self-mut-self-in-methods
+
+Summary table
+Short for Equivalent to Takes ownership? When to use? Example
+
+self self: Self a: A Yes The code that calls this method should not be able to reuse the object on which you call the method. Invalidate an object because maybe you transform it into another. Drop an object. Immutable data structures?
+
+&self self: &Self a: &A No - immutably borrows For reading self or its fields. For printing something or compute a value based on multiple values of other copyable fields.
+
+&mut self self: &mut Self a: &mut A No - mutable borrows For reading or writing self and its fields. A function that updates the internal state of a data structure.
+
+mut self mut self: Self mut a: A Yes If you want to change what self points to. Not useful because it takes ownership.
+
+https://jsdw.me/posts/rust-fn-traits/
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,231 @@
 use inheritance_macro::*;
 
-fn main() {
+trait IFoo {
+    fn foo_num(&mut self) -> &mut i32;
+    fn foo_str(&mut self) -> &mut String;
+    fn foo_bln(&mut self) -> &mut bool;
+}
+
+trait IBar {
+    fn bar_num(&mut self) -> &mut i32;
+    fn bar_str(&mut self) -> &mut String;
+    fn bar_bln(&mut self) -> &mut bool;
+}
+
+trait IPii: IFoo + IBar {
+    fn pii_num(&mut self) -> &mut i32;
+    fn pii_str(&mut self) -> &mut String;
+    fn pii_bln(&mut self) -> &mut bool;
+}
+
+struct Foo {
+    foo_num: i32,
+    foo_str: String,
+    foo_bln: bool,
+}
+
+struct Bar {
+    bar_num: i32,
+    bar_str: String,
+    bar_bln: bool,
+}
+
+struct Pii {
+    foo: Box<Foo>,
+    bar: Box<Bar>,
+    pii_num: i32,
+    pii_str: String,
+    pii_bln: bool,
+}
+
+impl IFoo for Foo {
+    fn foo_num(&mut self) -> &mut i32 {
+        &mut self.foo_num
+    }
+
+    fn foo_str(&mut self) -> &mut String {
+        &mut self.foo_str
+    }
+
+    fn foo_bln(&mut self) -> &mut bool {
+        &mut self.foo_bln
+    }
+}
+
+impl IBar for Bar {
+    fn bar_num(&mut self) -> &mut i32 {
+        &mut self.bar_num
+    }
+
+    fn bar_str(&mut self) -> &mut String {
+        &mut self.bar_str
+    }
+
+    fn bar_bln(&mut self) -> &mut bool {
+        &mut self.bar_bln
+    }
+}
+
+impl IFoo for Pii {
+    fn foo_num(&mut self) -> &mut i32 {
+        &mut self.foo.foo_num
+    }
+
+    fn foo_str(&mut self) -> &mut String {
+        &mut self.foo.foo_str
+    }
+
+    fn foo_bln(&mut self) -> &mut bool {
+        &mut self.foo.foo_bln
+    }
+}
+impl IBar for Pii {
+    fn bar_num(&mut self) -> &mut i32 {
+        &mut self.bar.bar_num
+    }
+
+    fn bar_str(&mut self) -> &mut String {
+        &mut self.bar.bar_str
+    }
+
+    fn bar_bln(&mut self) -> &mut bool {
+        &mut self.bar.bar_bln
+    }
+}
+impl IPii for Pii {
+    fn pii_num(&mut self) -> &mut i32 {
+        &mut self.pii_num
+    }
+
+    fn pii_str(&mut self) -> &mut String {
+        &mut self.pii_str
+    }
+
+    fn pii_bln(&mut self) -> &mut bool {
+        &mut self.pii_bln
+    }
+}
+
+// fn random_empty_tile(arr: &mut [Tile]) -> &mut Tile {
+//     let len = arr.len();
+//     let mut the_chosen_i = 0;
+//     loop {
+//         let i = rand::thread_rng().gen_range(0, len);
+//         let tile = &mut arr[i];
+//         if let Tile::Empty = tile {
+//             the_chosen_i = i;
+//             break;
+//         }
+//     }
+//     &mut arr[the_chosen_i]
+// }
+fn print_foo(
+    foo_num: i32,
+    foo_str: String,
+    foo_bln: bool,
+    bar_num: i32,
+    bar_str: String,
+    bar_bln: bool,
+    pii_num: i32,
+    pii_str: String,
+    pii_bln: bool,
+) {
+    println!(
+        "
+        foo_num: {}, foo_str: {}, foo_bln: {}, 
+        bar_num: {}, bar_str: {}, bar_bln: {}, 
+        pii_num: {}, pii_str: {}, pii_bln: {}",
+        foo_num, foo_str, foo_bln, bar_num, bar_str, bar_bln, pii_num, pii_str, pii_bln
+    );
+}
+fn test_foo() {
+    let foo_num: i32 = 10;
+    let foo_str: String = "foo i".to_string();
+    let foo_bln: bool = false;
+    let bar_num: i32 = 10;
+    let bar_str: String = "foo i".to_string();
+    let bar_bln: bool = false;
+    let pii_num: i32 = 10;
+    let pii_str: String = "foo i".to_string();
+    let pii_bln: bool = false;
+
+    let mut pii = Pii {
+        foo: Box::new(Foo {
+            foo_num: foo_num,
+            foo_str: foo_str,
+            foo_bln: foo_bln,
+        }),
+        bar: Box::new(Bar {
+            bar_num: bar_num,
+            bar_str: bar_str,
+            bar_bln: bar_bln,
+        }),
+        pii_num: pii_num,
+        pii_str: pii_str,
+        pii_bln: pii_bln,
+    };
+
+    let foo_num = pii.foo_num().clone();
+    let foo_str = pii.foo_str().clone();
+    let foo_bln = pii.foo_bln().clone();
+    let bar_num = pii.bar_num().clone();
+    let bar_str = pii.bar_str().clone();
+    let bar_bln = pii.bar_bln().clone();
+    let pii_num = pii.pii_num().clone();
+    let pii_str = pii.pii_str().clone();
+    let pii_bln = pii.pii_bln().clone();
+
+    print_foo(
+        foo_num,
+        foo_str.clone(),
+        foo_bln,
+        bar_num,
+        bar_str.clone(),
+        bar_bln,
+        pii_num,
+        pii_str.clone(),
+        pii_bln,
+    );
+
+    *pii.bar_bln() = match pii.bar_bln() {
+        true => false,
+        false => true,
+    };
+
+    *pii.foo_bln() = match pii.foo_bln() {
+        true => false,
+        false => true,
+    };
+
+    *pii.pii_bln() = match pii.pii_bln() {
+        true => false,
+        false => true,
+    };
+
+    *pii.foo_num() = 1000;
+    *pii.bar_num() = 1000;
+    *pii.pii_num() = 1000;
+
+    *pii.foo_str() = "foo ii".to_string();
+    *pii.bar_str() = "bar ii".to_string();
+    *pii.pii_str() = "pii ii".to_string();
+
+    let foo_num = pii.foo_num().clone();
+    let foo_str = pii.foo_str().clone();
+    let foo_bln = pii.foo_bln().clone();
+    let bar_num = pii.bar_num().clone();
+    let bar_str = pii.bar_str().clone();
+    let bar_bln = pii.bar_bln().clone();
+    let pii_num = pii.pii_num().clone();
+    let pii_str = pii.pii_str().clone();
+    let pii_bln = pii.pii_bln().clone();
+
+    print_foo(
+        foo_num, foo_str, foo_bln, bar_num, bar_str, bar_bln, pii_num, pii_str, pii_bln,
+    );
+}
+
+fn test_dragon() {
     // make_foo!(Animal, IAnimal, (given_name: String));
     // make_foo!(Bird, IBird, (wing_span: i32));
     // make_foo!(Lizard, ILizard, (number_claws: i32));
@@ -40,6 +265,10 @@ fn main() {
     dragon.fire();
 
     println!("Dragon fire capacity: {}", dragon.get_fire_capacity());
+}
+fn main() {
+    test_foo();
+    test_dragon();
 }
 
 //fn do_work_w_dragon(dragon: Dragon) {}
